09687126654fa7c61c93d21becc1f9da0e6b3b25
jatin <jatinkumarsahoo99@gmail.com>
2022-12-12 00:30:42 +0530

    by jatin

---
+++ humanflask.py
@@ -0,0 +1,86 @@
+
+#set FLASK_APP=humanflask.py
+#set FLASK_ENV =development
+#flask run --host=192.168.2.15
+
+
+
+
+from keras.models import load_model
+from flask import Flask ,jsonify,request
+import base64
+import io
+from keras_preprocessing.image import img_to_array
+import numpy as np
+import tensorflow as tf
+
+import time
+from keras_preprocessing.image import save_img
+from PIL import Image
+from flask_restful import Resource,Api,request
+
+app = Flask(__name__)
+api = Api(app)
+
+
+model = load_model('fully_trained.h5', compile=False)
+print('* Model Loaded')
+
+def prepare_image(image,target):
+    if image.mode != "RGB":
+        image = image.convert('RGB')
+    
+    image = image.resize(target)
+    image = img_to_array(image)
+    
+    image = (image -127.5) / 127.5
+    image = np.expand_dims(image,axis = 0)
+    return image
+
+
+
+class Predict(Resource):
+    def post(self):
+        json_data = request.get_json()
+        img_data = json_data['Image']
+        
+        image = base64.b64decode(str(img_data))
+        
+        img = Image.open(io.BytesIO(image))
+        
+        prepared_image = prepare_image(img,target=(256,256))
+        
+        preds = model.predict(prepared_image)
+        
+        outputfile = 'output.png'
+        
+        savePath='./output/'
+        
+        output =  tf.reshape(preds,[256,256,3])
+        
+        output = (output +1 )/2
+        
+        save_img(savePath+outputfile,img_to_array(output))
+        
+        
+        imageNew = Image.open(savePath+outputfile)
+        
+        imageNew = imageNew.resize((50,50))
+        
+        imageNew.save(savePath+"new_"+outputfile)
+        
+        with open(savePath+"new_"+outputfile,'rb') as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            
+        outputdata = {
+            'Image':str(encoded_string)
+        }  
+        
+        return outputdata
+
+
+api.add_resource(Predict,'/predict')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
